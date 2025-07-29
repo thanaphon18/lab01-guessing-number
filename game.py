dcf6095864b69bfd2aefde9be4ac677877d5a02a
Anatsaya2806 <139745300+Anatsaya2806@users.noreply.github.com>
2025-07-29 10:33:14 +0700

    Implement the check_player_guess function

--- game.py
+++ game.py
@@ -3,7 +3,16 @@
 
 def get_player_guess():
     pass
-   
+    while True:
+        try:
+            guess = int(input("Guess a number: "))
+            if 1 <= guess <= 100:
+                return guess
+            else:
+                print("Please enter a number between 1 and 100.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+    pass
 
 def check_guess(secret_number, player_guess):
     """
